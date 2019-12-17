ee753f029eba4dd0dde27700fd700123938ed851
patrick <patrick.ignoto@mail.mcgill.ca>
2019-12-16 21:47:33 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: patrick_ignoto
+"""
+
+import requests
+import flask
+import json
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/test/api/v1.0/jukeboxes/', methods=['GET'])
+def get_jukeboxes():
+    set_id =  flask.request.args.get("settingId")
+    model=  flask.request.args.get("model")
+    offset=  flask.request.args.get("offset")
+    limit=  flask.request.args.get("limit")
+    return "test {} {} {} {}".format(set_id, model, offset, limit)
+    
+def find_requirements(setting_id):
+    r = requests.get("http://my-json-server.typicode.com/touchtunes/tech-assignment/settings")
+    settings = r.json()
+    setting = [x for x in settings["settings"] if x["id"]==setting_id ]
+    requirements = setting["requires"]
+    return requirements
+    
+    
+if __name__ == '__main__':
+    app.run(debug=False)
